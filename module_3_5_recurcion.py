7c019c2ee64e17190506381e8ed91cd377355a42
sfilinkov7 <sfilinkov7@yandex.ru>
2024-09-24 13:06:30 +0500

    Add files via upload

--- module_3_5_recurcion.py
+++ module_3_5_recurcion.py
@@ -1,10 +1,11 @@
 def get_multiplied_digits(number):
     str_number = str(number)
     first = int(str_number[0])
-    if len(str_number) == 1:
-        return int(str_number)
+    if first == 0:
+        first = 1
     elif len(str_number) > 1:
         return first * get_multiplied_digits(int(str_number[1:]))
+    return first
 
 
 result = get_multiplied_digits(40203)
